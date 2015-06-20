d7b7afe4ba1c5e2751fe9dc51a47cf5d6aa0f76c
michaelhsilva <michaelhsilva2@gmail.com>
2015-06-20 16:22:32 -0300

    Game completed
    First version 1.0 finished

--- index.js
+++ index.js
@@ -10,30 +10,29 @@ app.get('/', function (req, res) {
 var server = app.listen(3000, function () {
 
   var port = server.address().port;
-  console.log('canvasgame webserver listening at %d', port);
+  console.log((new Date()) + ' canvasgame webserver listening at %d', port);
 
 });
 
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({ port: 3001 });
 
-console.log('canvasgame app listening at %d', 3001);
+console.log((new Date()) + ' canvasgame app listening at %d', 3001);
 
 // game variables
 var players = [];
-players[0] = {id:1, x:0,  y:0,  connected:false,color:'blue',clientId:-1};
-players[1] = {id:2, x:49, y:0,  connected:false,color:'red',clientId:-1};
-players[2] = {id:3, x:0,  y:29, connected:false,color:'yellow',clientId:-1};
-players[3] = {id:4, x:49, y:29, connected:false,color:'green',clientId:-1};
+players[0] = {id:1, x:0,   y:0,   previous_x:0,   previous_y:0,   connected:false,color:'blue',clientId:-1};
+players[1] = {id:2, x:490, y:0,   previous_x:490, previous_y:0,   connected:false,color:'red',clientId:-1};
+players[2] = {id:3, x:0,   y:290, previous_x:0,   previous_y:290, connected:false,color:'yellow',clientId:-1};
+players[3] = {id:4, x:490, y:290, previous_x:490, previous_y:290, connected:false,color:'green',clientId:-1};
 var allClients = [];
-var p = {id:1, x:0,  y:0, previous_x:0, previous_y:0, connected:false,color:'blue',clientId:1};
 
 wss.broadcast = function(data) {
   wss.clients.forEach(function each(client) {
 	try {
 		client.send(data);
 	} catch (e) {
-		console.log(e.message);
+		console.log((new Date()) + ' ' + e.message);
 	}
   });
 };
@@ -43,82 +42,84 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     var msg = JSON.parse(message);
   	var id = msg.data.id;
-  	//var player = null;
-    var player = p;
-  	/*for (i = 0; i < players.length; i++) {
-  	  player = players[i];
-  	  if (player.clientId == id)
+  	var player = null;
+  	for (i = 0; i < players.length; i++) {
+  	  var p = players[i];
+  	  if (p.clientId == id) {
+        player = p;
   		  break;
-    */
+      }
+    }
   	switch (msg.type) {
   		case "keypressed":
+        if (player == null)
+          return;
   			var keypressed = parseInt(msg.data.key);
         player.previous_x = player.x;
         player.previous_y = player.y;
   			if (keypressed == 37) { // LEFT
-  				console.log('player '+player.clientId+': LEFT');
+  				console.log((new Date()) + ' player '+player.clientId+': LEFT');
   				if (player.x != 0)
-  					player.x--;
+  					player.x-=10;
   			} else if (keypressed == 38) { // UP
-  				console.log('player '+player.clientId+': UP');
+  				console.log((new Date()) + ' player '+player.clientId+': UP');
   				if (player.y != 0)
-  					player.y--;
+  					player.y-=10;
   			} else if (keypressed == 39) { // RIGHT
-  				console.log('player '+player.clientId+': RIGHT');
+  				console.log((new Date()) + ' player '+player.clientId+': RIGHT');
   				if (player.x != 490)
-  					player.x++;
+  					player.x+=10;
   			} else if (keypressed == 40) { // DOWN
-  				console.log('player '+player.clientId+': DOWN');
+  				console.log((new Date()) + ' player '+player.clientId+': DOWN');
   				if (player.y != 290)
-  					player.y++;
+  					player.y+=10;
   			}
   			player.connected = true;
   			var newmsg = {type:1,data:player};
   			var data = JSON.stringify(newmsg);
-  			console.log(data);
   			wss.broadcast(data);
   			break;
   	}
   });
+  ws.on('close', function(reasonCode, description) {
+      var index = allClients.indexOf(ws);
+      var playerDisconnected = null;
+      for (i = 0; i < players.length; i++) {
+        playerDisconnected = players[i];
+        if (playerDisconnected.clientId == index) {
+          playerDisconnected.connected = false;
+          break;
+        }
+      }
+      var newmsg = {type:3,data:playerDisconnected};
+    	wss.broadcast(JSON.stringify(newmsg));
+      console.log((new Date()) + ' client id ' + index + ' disconnected.');
+  });
 
   allClients.push(ws);
   var index = allClients.indexOf(ws);
-  console.log('a user connected');
-  var newPlayer = p;
-  /*var newPlayer = null;
+  var newPlayer = null;
   for (i = 0; i < players.length; i++) {
 	  if (!players[i].connected) {
 		  players[i].clientId = index;
 		  newPlayer = players[i];
 		  break;
 	  }
-  }*/
+  }
   if (newPlayer == null) {
   	var newmsg = {type:2,data:'All available players are connected'};
-  	console.log(newmsg);
   	ws.send(JSON.stringify(newmsg));
   } else {
-  	console.log('new client id: %d', index);
-
+  	console.log((new Date()) + ' new client id: %d', index);
+    newPlayer.clientId = index;
   	var newmsg = {type:0,data:newPlayer};
-  	console.log(newmsg);
   	wss.broadcast(JSON.stringify(newmsg));
+    for (i = 0; i < players.length; i++) {
+      var otherPlayer = players[i];
+  	  if (otherPlayer.clientId != index && otherPlayer.connected) {
+        newmsg = {type:0,data:players[i]};
+      	wss.broadcast(JSON.stringify(newmsg));
+  	  }
+    }
   }
 });
-
-/*
-socket.on('disconnect', function(){
-	var i = allClients.indexOf(socket);
-	for (i = 0; i < players.length; i++) {
-	  var player = players[i];
-	  if (player.clientId == i) {
-		  player.clientId = -1;
-		  player.connected = false;
-		  io.emit('new position', JSON.stringfy(player));
-		  break;
-	  }
-    }
-    delete allClients[i];
-    console.log('user disconnected');
-  });
-*/
