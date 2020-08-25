bd754d3b470985c8a161b5b362b1931c29119c0f
michaelhsilva2@gmail.com <michael.angelo.silva@accenture.com>
2020-08-25 01:49:29 -0300

    refactoring

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,3 @@
-  $(function(){
-    $("#canvasWrapper").focus();
-  });
 
   var QueryString = function () {
     var query_string = {};
@@ -42,34 +39,61 @@
 
   ws.onmessage = function (event)
   {
-      var msg = JSON.parse(event.data);
-      var type = parseInt(msg['type']);
+      var msg = decodeMsg(event);
+      var type = getType(msg);
       
       switch (type) {
       case 0: // player connected
+        playerConnected(msg);
       case 1: // new position
-        var player = msg['data'];
-        if (clientId == null) {
-            clientId = player.clientId;
-        }
-        var myCanvas = document.getElementById("game");
-        var context = myCanvas.getContext("2d");
-
-        context.fillStyle = "white";
-        context.fillRect(player['previous_x'], player['previous_y'], 10, 10);
-        context.fillStyle = player['color'];
-        context.fillRect(player['x'], player['y'], 10, 10);
+        newPosition(msg);
         break;
       case 2: // max players reached
-        alert(msg['data']);
+        maxPlayersReached(msg);
         break;
       case 3: // player disconnected
-        var player = msg['data'];
-        var myCanvas = document.getElementById("game");
-        var context = myCanvas.getContext("2d");
-
-        context.fillStyle = "white";
-        context.fillRect(player['x'], player['y'], 10, 10);
+        playerDisconnected(msg);
         break;
       }
   };
+
+function decodeMsg(event) {
+  return JSON.parse(event.data);
+}
+
+function getType(msg) {
+  return parseInt(msg['type']);
+}
+
+function playerConnected(msg) {
+  console.log('A new player is connected.');
+}
+
+function newPosition(msg) {
+  var player = msg['data'];
+  if (clientId == null) {
+      clientId = player.clientId;
+  }
+
+  var context = getCanvasContext();
+  context.fillStyle = "white";
+  context.fillRect(player['previous_x'], player['previous_y'], 10, 10);
+  context.fillStyle = player['color'];
+  context.fillRect(player['x'], player['y'], 10, 10); 
+}
+
+function playerDisconnected(msg) {
+  var player = msg['data'];
+
+  var context = getCanvasContext();
+  context.fillStyle = "white";
+  context.fillRect(player['x'], player['y'], 10, 10);
+}
+
+function maxPlayersReached(msg) {
+  alert('Max number of players reached ' + msg['data']);
+}
+
+function getCanvasContext() {
+  return document.getElementById("game").getContext('2d');
+}
