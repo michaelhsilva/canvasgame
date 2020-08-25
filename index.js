b8ea2786f034674b68028ec0558e1a393689ee82
michaelhsilva2@gmail.com <michael.angelo.silva@accenture.com>
2020-08-25 00:02:02 -0300

    adding server url

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var server = app.listen(3000, function () {
 
   var port = server.address().port;
   console.log((new Date()) + ' canvasgame webserver listening at %d', port);
+  console.log((new Date()) + ' http://localhost:%d', port);
 
 });
 
