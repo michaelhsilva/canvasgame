c1cb4495d033ca73f15a69291482e4f9e06ad782
michaelhsilva2@gmail.com <michael.angelo.silva@accenture.com>
2020-08-25 01:31:05 -0300

    format

--- public/js/client.js
+++ public/js/client.js
@@ -26,49 +26,50 @@
   if (QueryString.host)
     hostnameOrIP = QueryString.host;
 
-      var ws = new WebSocket('ws://'+hostnameOrIP+':3001');
+  var ws = new WebSocket('ws://'+hostnameOrIP+':3001');
 
-      ws.onopen = function (event) {
-          // Connection opened
-      }
+  ws.onopen = function (event) {
+      // Connection opened
+  }
 
-      $("#canvasWrapper").keydown(function(e) {
-          var msg = {
-              type: "keypressed",
-              data: {id:clientId, key:e.which}
-          };
-          ws.send(JSON.stringify(msg));
-      });
-
-      ws.onmessage = function (event)
-      {
-          var msg = JSON.parse(event.data);
-          var type = parseInt(msg['type']);
-          switch(type) {
-          case 0: // player connected
-          case 1: // new position
-              var player = msg['data'];
-              if (clientId == null) {
-                  clientId = player.clientId;
-              }
-              var myCanvas = document.getElementById("game");
-              var context = myCanvas.getContext("2d");
-
-              context.fillStyle = "white";
-              context.fillRect(player['previous_x'], player['previous_y'], 10, 10);
-              context.fillStyle = player['color'];
-              context.fillRect(player['x'], player['y'], 10, 10);
-            break;
-          case 2: // max players reached
-            alert(msg['data']);
-            break;
-          case 3: // player disconnected
-            var player = msg['data'];
-            var myCanvas = document.getElementById("game");
-            var context = myCanvas.getContext("2d");
-
-            context.fillStyle = "white";
-            context.fillRect(player['x'], player['y'], 10, 10);
-            break;
-          }
+  $("#canvasWrapper").keydown(function(e) {
+      var msg = {
+          type: "keypressed",
+          data: {id:clientId, key:e.which}
       };
+      ws.send(JSON.stringify(msg));
+  });
+
+  ws.onmessage = function (event)
+  {
+      var msg = JSON.parse(event.data);
+      var type = parseInt(msg['type']);
+      
+      switch (type) {
+      case 0: // player connected
+      case 1: // new position
+        var player = msg['data'];
+        if (clientId == null) {
+            clientId = player.clientId;
+        }
+        var myCanvas = document.getElementById("game");
+        var context = myCanvas.getContext("2d");
+
+        context.fillStyle = "white";
+        context.fillRect(player['previous_x'], player['previous_y'], 10, 10);
+        context.fillStyle = player['color'];
+        context.fillRect(player['x'], player['y'], 10, 10);
+        break;
+      case 2: // max players reached
+        alert(msg['data']);
+        break;
+      case 3: // player disconnected
+        var player = msg['data'];
+        var myCanvas = document.getElementById("game");
+        var context = myCanvas.getContext("2d");
+
+        context.fillStyle = "white";
+        context.fillRect(player['x'], player['y'], 10, 10);
+        break;
+      }
+  };
